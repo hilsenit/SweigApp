4aff5ad1b9c6d3318cf7b9a402fcc45cd3deda68
peterhoejlundandersen <hilsenit@gmail.com>
2018-05-02 14:04:32 +0200

    added delegate function, so letters can be shown and clicked

--- app/AlphabetController.rb
+++ app/AlphabetController.rb
@@ -5,21 +5,36 @@ class AlphabetController < UIViewController
     self.title = "Alphabet"
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     self.view.addSubview @table
-    # @table.dataSource = self
+    @table.dataSource = self
+    @table.delegate = self
+    @data = ("A".."Z").to_a
 
   end
 
-  # def tableView(tableView, cellForRowAtIndexPath: indexPath)
-  #   @reuseIdentifier ||= "CELL_IDENTIFIER"
+  def tableView(tableView, didSelectRowAtIndexPath: indexPath)
+    # Ellers bliver den row ved med at være markeret
+    tableView.deselectRowAtIndexPath(indexPath, animated: true)
 
-  #   cell = tableView.dequeReusableCellWithIdentifier(@reuseIdentifier) || begin
-  #     UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: @reuseIdentifier)
-  #   end
+    alert = UIAlertView.alloc.init #Har ikke brug for nogen størrelse
+    alert.message = "#{@data[indexPath.row]} tapped!"
+    alert.addButtonWithTitle "Ok"
+    alert.show
+  end
 
-  #   cell
-  # end
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
 
-  # def tableView(tableView, numberOfRowsInSection: section)
+    # Kigger efter, om der allerede er data gemt med identifieren! - hvis ikke, så lav en ny
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier: @reuseIdentifier)
+    end
 
-  # end
+    cell.textLabel.text = @data[indexPath.row]
+
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @data.count
+  end
 end
